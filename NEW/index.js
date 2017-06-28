85d4fe0bdf393213e449b20fafa803339f185232
kevinbae15 <kooddog15@gmail.com>
2017-06-28 18:51:38 -0400

    animations

--- NEW/index.js
+++ NEW/index.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
-  $('#intro').hide();
-  $('#intro').hide();
-  $("#intro").fadeIn(1500);
+  $('.nav_link').hide();
+  $('.intro').hide();
+  $(".intro").fadeIn(1500);
+  $(".nav_link").fadeIn(1500);
+  
+  $("#E").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#page2").offset().top
+    }, 1500);
+  });
 })
      
