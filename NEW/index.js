8341f58f66234e7f4e100cdddc6f5ec045c5de90
kevinbae15 <kooddog15@gmail.com>
2017-06-29 03:39:09 -0400

    made homepage look nicer

--- NEW/index.js
+++ NEW/index.js
@@ -1,8 +1,14 @@
+window.onload = function() {
+    $('html, body').animate({
+        scrollTop: $("#page1").offset().top + 80
+    }, 1200);
+};
+
 $(document).ready(function(){
   $('.nav_link').hide();
   $('.intro').hide();
-  $(".intro").fadeIn(1500);
-  $(".nav_link").fadeIn(1500);
+  $(".intro").fadeIn(2000);
+  $(".nav_link").fadeIn(2000);
   
   $("#E").click(function() {
     $('html, body').animate({
