122bd7b30224b167b2c51cfe914f79e669aba034
kevinbae15 <kooddog15@gmail.com>
2017-07-01 05:43:29 -0400

    Added new font + new animations + added logo

--- NEW/index.js
+++ NEW/index.js
@@ -7,6 +7,8 @@ window.onload = function() {
 $(document).ready(function(){
   $('.nav_link').hide();
   $('.intro').hide();
+  $('#logo').hide();
+  $('#logo').fadeIn(1700);
   $(".intro").fadeIn(1700);
   $(".nav_link").fadeIn(1700);
   
@@ -58,7 +60,47 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
-})
+
+
+   //window and animation items
+  var animation_elements = $.find('.animation-element');
+  var web_window = $(window);
+
+  //check to see if any animation containers are currently in view
+  function check_if_in_view() {
+    //get current window information
+    var window_height = web_window.height();
+    var window_top_position = web_window.scrollTop();
+    var window_bottom_position = (window_top_position + window_height);
+
+    //iterate through elements to see if its in view
+    $.each(animation_elements, function() {
+
+      //get the element sinformation
+      var element = $(this);
+      var element_height = $(element).outerHeight();
+      var element_top_position = $(element).offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is visible (its viewable if it exists between the viewable space of the viewport)
+      if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+        element.addClass('in-view');
+      } else {
+        element.removeClass('in-view');
+      }
+    });
+
+  }
+
+  //on or scroll, detect elements in view
+  $(window).on('scroll resize', function() {
+      check_if_in_view()
+    })
+    //trigger our scroll event on initial load
+  $(window).trigger('scroll');
+
+  
+});
 
 var slideIndex = 1;
 showSlides(slideIndex);
